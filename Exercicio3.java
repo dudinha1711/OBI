2b4cf7b34bce4b5b9ede39ea412d84e3dc992fc3
Maria Eduarda <duda.desenvolvedor@gmail.com>
2025-05-12 14:42:13 -0300

    Resolução exercicio 3

---
+++ Exercicio3.java
@@ -0,0 +1,14 @@
+import java.util.Scanner;
+
+public class Exercicio3 {
+    public static void main(String[] args) {
+        Scanner scanner = new Scanner(System.in);
+
+        System.out.print("Digite uma palavra: ");
+        String palavra = scanner.nextLine();
+
+        for (int i = 0; i < palavra.length(); i++) {
+            System.out.println(palavra.charAt(i));
+        }
+    }
+}
